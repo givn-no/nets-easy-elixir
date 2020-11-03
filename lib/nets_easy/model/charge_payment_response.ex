c52a0b76c00a9083b20a4cd4736f105ffc26ca06
Håvard Lindset <havard@hoopla.no>
2020-11-03 14:24:03 +0100

    add charge api

---
+++ lib/nets_easy/model/charge_payment_response.ex
@@ -0,0 +1,20 @@
+defmodule NetsEasy.Model.ChargePaymentResponse do
+  @moduledoc """
+  """
+
+  @typedoc """
+  Root response for charging a payment.
+
+  See https://tech.dibspayment.com/easy/api/paymentapi#chargePayment
+  """
+  @type t :: %__MODULE__{
+    charge_id: String.t()
+  }
+  @derive Poison.Encoder
+  defstruct [
+    :charge_id
+  ]
+
+  @doc false
+  def shell(), do: %__MODULE__{}
+end
