0dd81f078a2f57034465874e942412fcb4cf16c1
Halvor Granskogen Bjørnstad <halvorgb@gmail.com>
2020-11-20 15:21:31 +0100

    refunds is a list? not according to docs.

--- lib/nets_easy/model/get_order_response/payment.ex
+++ lib/nets_easy/model/get_order_response/payment.ex
@@ -15,7 +15,7 @@ defmodule NetsEasy.Model.GetOrderResponse.Payment do
           order_details: Model.GetOrderResponse.OrderDetails.t(),
           payment_details: Model.GetOrderResponse.PaymentDetails.t(),
           payment_id: String.t(),
-          refunds: Model.GetOrderResponse.Refunds.t(),
+          refunds: [Model.GetOrderResponse.Refunds.t()],
           summary: Model.GetOrderResponse.Summary.t(),
           subscription: Model.GetOrderResponse.Subscription.t()
         }
@@ -42,7 +42,7 @@ defmodule NetsEasy.Model.GetOrderResponse.Payment do
       consumer: Model.GetOrderResponse.Consumer.shell(),
       order_details: Model.GetOrderResponse.OrderDetails.shell(),
       payment_details: Model.GetOrderResponse.PaymentDetails.shell(),
-      refunds: Model.GetOrderResponse.Refunds.shell(),
+      refunds: [Model.GetOrderResponse.Refunds.shell()],
       summary: Model.GetOrderResponse.Summary.shell(),
       subscription: Model.GetOrderResponse.Subscription.shell()
     }
